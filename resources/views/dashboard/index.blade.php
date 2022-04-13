ed6f7e76c3a79d7cbe2cb03a56cd2e34ef9fa603
tzelginianc <tzelginia78@gmail.com>
2022-04-13 22:11:26 +0700

    progressan proyek cuci mobil

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,11 @@
+@extends('dashboard.layout.main')
+
+@section('content')
+    
+<div class="content-wrapper">
+  <div class="d-xl-flex justify-content-between align-items-start">
+    <h2 class="text-dark font-weight-bold mb-2">Dashboard </h2>
+  </div>
+</div>
+
+@endsection
---
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -0,0 +1,32 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+      <li class="nav-item nav-category">Main</li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          <span class="icon-bg"><i class="mdi mdi-cube menu-icon"></i></span>
+          <span class="menu-title">Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+          <span class="icon-bg"><i class="mdi mdi-crosshairs-gps menu-icon"></i></span>
+          <span class="menu-title">UI Elements</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-basic">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/icons/mdi.html">
+          <span class="icon-bg"><i class="mdi mdi-contacts menu-icon"></i></span>
+          <span class="menu-title">Icons</span>
+        </a>
+      </li>
+
+    </ul>
+  </nav>
