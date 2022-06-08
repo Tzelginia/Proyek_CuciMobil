c0fa7b0188ca1cb70e60382b1ab72f73f8721bc5
tzelginianc <tzelginia78@gmail.com>
2022-06-08 19:01:05 +0700

    Menambahkan function verify dan block

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,40 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('login.index', [
             'title' => 'Login'
         ]);
     }
 
-    public function authenticate(Request $request){
-        $credentials = $request -> validate([
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
             'username' => 'required',
             'password' => 'required'
         ]);
-        
-        if(Auth::attempt($credentials)){
+
+        if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            
-            return redirect()->intended('/');
+            if (Auth::user()->verify === 0) {
+                Auth::logout();
+                return back()->with('loginError', 'Silahkan hubungi administrator untuk aktivasi akun anda agar bisa masuk');
+            }
+            return redirect()->intended('/dashboard');
         }
 
         return back()->with('loginError', 'Gagal Login');
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         Auth::logout();
 
         $request->session()->invalidate();
 
         $request->session()->regenerate();
-        
-        return redirect('/login');
 
+        return redirect('/login');
+    }
+    public function verify(Request $request, User $user)
+    {
+
+        $user = User::find($request)->first();
+        if ($user) {
+            $user->verify = '1';
+            $user->save();
+        }
+
+        return redirect('/dashboard/pelanggan');
+    }
+    public function block(Request $request)
+    {
+
+        $user = User::find($request)->first();
+        if ($user) {
+            $user->verify = '0';
+            $user->save();
+        }
+
+        return redirect('/dashboard/pelanggan');
     }
 }
