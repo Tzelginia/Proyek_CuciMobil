b41abb257658ef3bb6ed6bbad96b9650baacf837
tzelginianc <tzelginia78@gmail.com>
2022-06-08 19:04:09 +0700

    index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+   @extends('layout.main')
+   @section('content')
+   <!-- Page Content -->
+   <!-- Banner Starts Here -->
+   <div class="banner">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-12">
+                   <div class="caption">
+                       <h2>Selamat Datang Di Web CuMil</h2>
+                       <div class="main-button">
+                           <a href="#">Order Now!</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+   @endsection
