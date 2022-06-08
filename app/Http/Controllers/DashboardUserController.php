8d0b9a256f47e822f566cd986e7af665dfa4e784
tzelginianc <tzelginia78@gmail.com>
2022-06-08 19:00:33 +0700

    DashboardUserController

---
+++ app/Http/Controllers/DashboardUserController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class DashboardUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('dashboard.pelanggan.index', [
+            'title' => 'pelanggan',
+            'user' => User::paginate(2)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::where('id', $id)->first();
+        return view('dashboard.pelanggan.show', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::where('id', $id)->first();
+        return view('dashboard.pelanggan.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'username' => 'required',
+            'nohp' => 'required|numeric',
+            'alamat' => 'required',
+        ]);
+        $user = User::where('id', $id)->first();
+        $user->name = $request->get('name');
+        $user->username = $request->get('username');
+        $user->nohp = $request->get('nohp');
+        $user->alamat = $request->get('alamat');
+        $user->save();
+        return redirect('/dashboard/pelanggan')
+            ->with('success', 'Data Berhasil Diupdate');
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
